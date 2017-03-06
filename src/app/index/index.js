988b2672a39e1fe5ad5662dbf878df8f9eed9801
Nick Collings <nick@collingo.com>
2017-03-06 12:08:27 +0000

    Add job actions, tidy job page layout

--- src/app/index/index.js
+++ src/app/index/index.js
@@ -39,6 +39,28 @@ export default () => (
             </a>
           </li>
         </ul>
+        <div className={style.actions}>
+          <div className={style.action}>
+            <p>Interested? It only takes 2 seconds to apply & you don’t need a CV.</p><a className={style.apply} href='#'>Apply</a>
+          </div>
+          <div className={style.action}>
+            <p>Know someone perfect? We’ll give you £X,XXX if they get the job.</p><a className={style.nudj} href='#'>Nudj</a>
+          </div>
+        </div>
+        <div className={style.related}>
+          <h2 className={style.relatedTitle}>Other positions</h2>
+          <ul>
+            <li>
+              <a href="#">Job Title, Location</a>
+            </li>
+            <li>
+              <a href="#">Job Title, Location</a>
+            </li>
+            <li>
+              <a href="#">Job Title, Location</a>
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
   </div>
