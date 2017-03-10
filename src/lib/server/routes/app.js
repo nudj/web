6137e99ab0b57a21d2716ff8bd92c42b7b009a6e
Nick Collings <nick@collingo.com>
2017-03-10 13:28:30 +0000

    Integrate data fetching based on url

--- src/lib/server/routes/app.js
+++ src/lib/server/routes/app.js
@@ -5,28 +5,33 @@ let build = require('../build').default
 let router = express.Router()
 
 router.get('*', (req, res) => {
-
-  fetch('http://127.0.0.1:3001/jobs/1')
-    .then((response) => response.json())
-    .then((job) => {
-      let data = {
+  let path = req.path.split('/')
+  let dataPromise
+  if (path.length === 3 && path[1] === 'jobs') {
+    dataPromise = fetch(`http://127.0.0.1:3001/jobs/${path[2]}`)
+      .then((response) => response.json())
+      .then((job) => ({
         page: {
           job
         }
-      }
-      let renderResult = build(data, req.url)
-      if (renderResult.url) {
-        res.writeHead(302, {
-          Location: renderResult.url
-        })
-        res.end()
-      } else {
-        return res.render('app', {
-          data: JSON.stringify(data),
-          html: renderResult
-        })
-      }
-    })
+      }))
+  } else {
+    dataPromise = Promise.resolve({})
+  }
+  dataPromise.then((data) => {
+    let renderResult = build(data, req.url)
+    if (renderResult.url) {
+      res.writeHead(302, {
+        Location: renderResult.url
+      })
+      res.end()
+    } else {
+      return res.render('app', {
+        data: JSON.stringify(data),
+        html: renderResult
+      })
+    }
+  })
 })
 
 module.exports = router
