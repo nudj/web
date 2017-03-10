90d57e28a1f16ec60e979c618ff093ce3790ffb6
Nick Collings <nick@collingo.com>
2017-03-10 13:14:39 +0000

    Add mock api integration

--- src/lib/app/components/job-page/job-page.js
+++ src/lib/app/components/job-page/job-page.js
@@ -14,7 +14,7 @@ export default (props) => (
       <img className={style.logo} src='' />
       <h1 className={style.title}>{get(props, 'job.title')}</h1>
       <h2 className={style.location}>{get(props, 'job.location')}</h2>
-      <h2 className={style.salary}>{get(props, 'job.salary')}</h2>
+      <h2 className={style.salary}>£{get(props, 'job.remuneration') * 1000}</h2>
       <ul className={style.links}>
         <li className={style.link}>
           <a href={get(props, 'job.company.url')}>View company website</a>
@@ -48,7 +48,7 @@ export default (props) => (
           <p>Interested? It only takes 2 seconds to apply & you don’t need a CV.</p><a className={style.apply} href='#'>Apply</a>
         </div>
         <div className={style.action}>
-          <p>Know someone perfect? We’ll give you £X,XXX if they get the job.</p><a className={style.nudj} href='#'>Nudj</a>
+          <p>Know someone perfect? We’ll give you £{get(props, 'job.bonus')} if they get the job.</p><a className={style.nudj} href='#'>Nudj</a>
         </div>
       </div>
       <div className={style.related}>
--- src/lib/server/routes/app.js
+++ src/lib/server/routes/app.js
@@ -1,52 +1,32 @@
 let express = require('express')
-let build = require('../build').default
+let fetch = require('node-fetch')
 
+let build = require('../build').default
 let router = express.Router()
 
 router.get('*', (req, res) => {
-  let data = {
-    page: {
-      job: {
-        title: `Job title ${req.path.split('/')[2]}`,
-        location: 'Location 1',
-        salary: 'Salary 1',
-        url: '#jobUrl',
-        company: {
-          url: '#companyUrl'
-        },
-        related: [
-          {
-            id: 2,
-            title: 'Job title 2',
-            location: 'Location 2'
-          },
-          {
-            id: 3,
-            title: 'Job title 3',
-            location: 'Location 3'
-          },
-          {
-            id: 4,
-            title: 'Job title 4',
-            location: 'Location 4'
-          }
-        ]
+
+  fetch('http://127.0.0.1:3001/jobs/1')
+    .then((response) => response.json())
+    .then((job) => {
+      let data = {
+        page: {
+          job
+        }
       }
-    }
-  }
-  let renderResult = build(data, req.url)
-  console.log('renderResult', renderResult)
-  if (renderResult.url) {
-    res.writeHead(302, {
-      Location: renderResult.url
+      let renderResult = build(data, req.url)
+      if (renderResult.url) {
+        res.writeHead(302, {
+          Location: renderResult.url
+        })
+        res.end()
+      } else {
+        return res.render('app', {
+          data: JSON.stringify(data),
+          html: renderResult
+        })
+      }
     })
-    res.end()
-  } else {
-    return res.render('app', {
-      data: JSON.stringify(data),
-      html: renderResult
-    })
-  }
 })
 
 module.exports = router
