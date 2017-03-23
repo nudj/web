bbbfbe24977e7880c2d2031782de948eca46d642
jgunson <jamie.gunson@googlemail.com>
2017-03-23 18:36:03 +0000

    initial styling

--- src/lib/app/components/job-page/job-page.js
+++ src/lib/app/components/job-page/job-page.js
@@ -9,48 +9,54 @@ function get (object, path, fallback) {
 }
 
 export default (props) => (
-  <div className={style.container}>
-    <div className={style.job}>
+  <div className={style.section}>
+    <div className={style.container}>
+      <div className={style.job}>
       <img className={style.logo} src={get(props, 'company.logo')} />
       <h1 className={style.title}>{get(props, 'job.title')}</h1>
       <h2 className={style.location}>{get(props, 'job.location')}</h2>
       <h2 className={style.salary}>£{get(props, 'job.remuneration') * 1000}</h2>
-      <ul className={style.links}>
-        <li className={style.link}>
+      <hr className={style.breakLine}></hr>
+      <div className={style.links}>
+        <div className={style.link}>
           <a href={get(props, 'company.url', '#company-url')}>View company website</a>
-        </li>
-        <li className={style.link}>
-          <a href={get(props, 'job.url', '#job-url')}>View full job post</a>
-        </li>
-        <li className={style.social}>
-          <a className={style.socialLink} href='#'>
-            <i className={style.facebook} />
-          </a>
-        </li>
-        <li className={style.social}>
-          <a className={style.socialLink} href='#'>
-            <i className={style.twitter} />
-          </a>
-        </li>
-        <li className={style.social}>
-          <a className={style.socialLink} href='#'>
-            <i className={style.linkedin} />
-          </a>
-        </li>
-        <li className={style.social}>
-          <a className={style.socialLink} href='#'>
-            <i className={style.instagram} />
-          </a>
-        </li>
-      </ul>
-      <div className={style.actions}>
-        <div className={style.action}>
-          <p>Interested? It only takes 2 seconds to apply & you don’t need a CV.</p><a className={style.apply} href='#'>Apply</a>
         </div>
-        <div className={style.action}>
-          <p>Know someone perfect? We’ll give you £{get(props, 'job.bonus')} if they get the job.</p><a className={style.nudj} href='#'>Nudj</a>
+        <div className={style.link}>
+          <a href={get(props, 'job.url', '#job-url')}>View full job post</a>
+        </div>
+        <div className={style.social}>
+          <div className={style.socialLink}>
+            <a href='#'>
+              <i className={style.facebook} />
+            </a>
+          </div>
+          <div className={style.socialLink}>
+            <a href='#'>
+              <i className={style.twitter} />
+            </a>
+          </div>
+          <div className={style.socialLink}>
+            <a href='#'>
+              <i className={style.linkedin} />
+            </a>
+          </div>
+          <div className={style.socialLink}>
+            <a href='#'>
+              <i className={style.instagram} />
+            </a>
+          </div>
         </div>
       </div>
+      <hr className={style.breakLine}></hr>
+      <div className={style.actions}>
+        <div className={style.action}>
+          <p>Interested? It only takes <strong>2 seconds</strong> to apply & you don’t need a CV.</p><a className={style.apply} href='#'>Apply</a>
+        </div>
+        <div className={style.action}>
+          <p>Know someone perfect? We’ll give you <strong>£{get(props, 'job.bonus')}</strong> if they get the job.</p><a className={style.nudj} href='#'>Nudj</a>
+        </div>
+      </div>
+      <hr className={style.breakLine}></hr>
       <div className={style.related}>
         <h2 className={style.relatedTitle}>Other positions</h2>
         <Link to={`/`}>Home</Link>
@@ -61,5 +67,6 @@ export default (props) => (
         </ul>
       </div>
     </div>
+  </div>
   </div>
 )
