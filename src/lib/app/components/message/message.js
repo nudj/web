925a350f680b519f960c5365f74723de1d110456
jgunson <jamie.gunson@googlemail.com>
2017-04-07 16:14:39 +0100

    Making edits for v1 - ch121

--- src/lib/app/components/message/message.js
+++ src/lib/app/components/message/message.js
@@ -2,6 +2,10 @@ import React from 'react'
 import get from 'lodash/get'
 import style from './message.css'
 
-export default (props) => {
-  return <div className={style[`${get(props, 'message.type')}Message`]}>{get(props, 'message.message')}</div>
+export default(props) => {
+  return <div className={style.wrapper}>
+    <div className={style[`${get(props, 'message.type')}Message`]}>
+      <div className={style.copy}>{get(props, 'message.message')}</div>
+    </div>
+  </div>
 }
--- src/lib/app/components/success-page/success-page.js
+++ src/lib/app/components/success-page/success-page.js
@@ -5,7 +5,7 @@ export default (props) => (
   <div className={style.body}>
     <div className={style.hero}>
       <div className={style.heroBody}>
-        <iframe src='//giphy.com/embed/26ufmAlKt4ne2JDnq?html5=true' width='300' height='300' frameBorder='0' />
+        <img src='https://media.giphy.com/media/26ufmAlKt4ne2JDnq/giphy.gif' width='320' height='320' />
         <p className={style.header}>Nice one!</p>
         <p className={style.copy}>Someone from our team will be in touch shortly.</p>
       </div>
