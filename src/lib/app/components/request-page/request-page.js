7c95ad2ca11a262cdc321f9c8bf0daf7e229f23f
Wayne Durack <wayne.durack@gmail.com>
2017-04-28 16:26:10 +0100

    Signup and request page

--- src/lib/app/components/request-page/request-page.js
+++ src/lib/app/components/request-page/request-page.js
@@ -8,38 +8,44 @@ const Component = (props) => {
   if (props.success) {
     html = (
       <div className={style.content}>
-        <img src='https://media.giphy.com/media/26ufmAlKt4ne2JDnq/giphy.gif' width='320' height='320' />
-        <p className={style.successHeader}>Nice one!</p>
-        <p className={style.successCopy}>Someone from our team will be in touch shortly.</p>
+        <div className={style.formHeader}>
+          <h1 className={style.title}>Nice one!</h1>
+          <p className={style.subtitle}>Someone from our team will be in touch shortly.</p>
+          <img className={style.okHand} src='/assets/images/ok-hand.svg' alt='Ok' />
+        </div>
       </div>
     )
   } else {
     html = (
-      <form className={style.content} action='/request' method='post'>
+      <form className={style.content} action='/signup' method='post'>
         <input type='hidden' name='_csrf' value={props.csrfToken} />
         <div className={style.formHeader}>
-          <h2 className={style.title}>Good things come to those who... nudj</h2>
+          <h1 className={style.title}>Great jobs come to those who... nudj</h1>
           <p className={style.subtitle}>Just enter your details below and we'll get back to you as soon as possible.</p>
         </div>
-        <fieldset className={style.fieldSet} id='request'>
-          <div className={style.fieldWrap}>
-            <label className={style.fieldLabel} for='first_name'>First Name</label>
-            <input className={style.fieldInput} id='first_name' name='first_name' required type='text' />
+        <fieldset className={style.fieldSet} id='sign_up'>
+          <div className={style.fieldWrapContainer}>
+            <div className={style.fieldWrapShortOdd}>
+              <label className={style.fieldLabel} htmlFor='first_name'>First Name</label>
+              <input className={style.fieldInput} id='first_name' name='first_name' required type='text' />
+            </div>
+            <div className={style.fieldWrapShortEven}>
+              <label className={style.fieldLabel} htmlFor='last_name'>Last Name</label>
+              <input className={style.fieldInput} id='last_name' name='last_name' required type='text' />
+            </div>
+            <div className={style.fieldWrap}>
+              <label className={style.fieldLabel} htmlFor='email_address'>Email</label>
+              <input className={style.fieldInput} id='email' name='email' required type='text' />
+            </div>
+            <div className={style.fieldWrap}>
+              <label className={style.fieldLabel} htmlFor='company_name'>Company</label>
+              <input className={style.fieldInput} id='company_name' name='company_name' type='text' />
+            </div>
           </div>
-          <div className={style.fieldWrap}>
-            <label className={style.fieldLabel} for='last_name'>Last Name</label>
-            <input className={style.fieldInput} id='last_name' name='last_name' required type='text' />
-          </div>
-          <div className={style.fieldWrap}>
-            <label className={style.fieldLabel} for='email_address'>Email</label>
-            <input className={style.fieldInput} id='email' name='email' required type='text' />
-          </div>
-          <div className={style.fieldWrap}>
-            <label className={style.fieldLabel} for='company'>Company</label>
-            <input className={style.fieldInput} id='company_name' name='company_name' type='text' />
+          <div className={style.fieldButtons}>
+            <input className={style.fieldSubmit} type='submit' value='Request access' />
           </div>
         </fieldset>
-        <input className={style.fieldSubmit} type='submit' value='Request access' />
       </form>
     )
   }
