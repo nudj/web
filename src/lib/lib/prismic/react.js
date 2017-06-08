e92cd22c8f8d0b9c06475a648e39be4fd57987f9
Wayne Durack <wayne.durack@gmail.com>
2017-06-08 12:24:24 +0100

    Tidy up

--- src/lib/lib/prismic/react.js
+++ src/lib/lib/prismic/react.js
@@ -1,21 +1,9 @@
-const React = require('react')
 const Prismic = require('prismic.io')
-const omit = require('lodash/omit')
 
 class PrismicReact {
   constructor (doc) {
     this.doc = this.convertDocument(doc)
   }
-
-  // createElement ({block, props, index}) {
-  //   // `block.type` contains information about the element type
-  //   if (typeof props.element === 'object' && props.element.$$typeof) {
-  //     return (<props.element.type {...omit(props.element.props, ['element'])} {...omit(props, ['element'])} key={`${props.element.type}+${index}`}>{block.text}</props.element.type>)
-  //   } else {
-  //     return (<props.element {...omit(props, ['element'])} key={`${props.element}+${index}`}>{block.text}</props.element>)
-  //   }
-  //   // add a `key` property?
-  // }
 
   convertDocument (doc) {
     if (doc.get) {
@@ -25,24 +13,6 @@ class PrismicReact {
     const prismicDoc = new Prismic.Document(id, uid, type, href, tags, slugs, firstPublicationDate, lastPublicationDate, lang, alternateLanguages, data, rawJSON)
     return prismicDoc
   }
-
-  // fragmentToReact ({fragment, props}) {
-  //   const structuredText = this.doc.get(fragment)
-  //
-  //   if (!structuredText) {
-  //     console.log('FRAGMENT NOT FOUND', fragment)
-  //     return []
-  //   }
-  //
-  //   if (!structuredText.blocks || !Array.isArray(structuredText.blocks)) {
-  //     console.log('HAS NO BLOCKS') // should return this?
-  //     return []
-  //   }
-  //
-  //   const reactElements = structuredText.blocks.map((block, index) => this.createElement({block, props, index}))
-  //
-  //   return reactElements
-  // }
 
   fragmentToText ({fragment}) {
     const prismicFragment = this.doc.get(fragment)
