4996ea473c4c089a342d17bcc75cfef17ec0088b
Wayne Durack <wayne.durack@gmail.com>
2017-07-18 13:23:22 +0100

    Couple of patches

--- src/lib/app/components/animate-appearance/animate-appearance.js
+++ src/lib/app/components/animate-appearance/animate-appearance.js
@@ -16,11 +16,13 @@ class Component extends React.Component {
   }
 
   componentDidMount () {
+    window.addEventListener('resize', this.handleScroll.bind(this))
     window.addEventListener('scroll', this.handleScroll.bind(this))
     window.requestAnimationFrame(() => this.handleScroll())
   }
 
   componentWillUnmount () {
+    window.removeEventListener('resize', this.handleScroll.bind(this))
     window.removeEventListener('scroll', this.handleScroll.bind(this))
   }
 
@@ -61,7 +63,7 @@ class Component extends React.Component {
     const disappearClass = this.style[`${disappearDirection}Disappear`]
     const visibleClass = this.state.visible ? this.style.appear : disappearClass
 
-    const inheritedClass = get(this.props, 'className', {})
+    const inheritedClass = get(this.props, 'className', '')
 
     return (<div className={`${visibleClass} ${inheritedClass}`} ref='wrapper'>{this.props.children}</div>)
   }
