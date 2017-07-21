600c2c74e33ff8f4c8d7e8e3249bfef9ffc148f4
Wayne Durack <wayne.durack@gmail.com>
2017-07-21 16:45:45 +0100

    Related job jiggling

--- src/lib/app/components/job-page/job-page.css.js
+++ src/lib/app/components/job-page/job-page.css.js
@@ -99,7 +99,8 @@ const styles = {
     padding: variables.padding.c,
     [mixins.breakpoints.l]: {
       display: 'flex',
-      padding: `${variables.padding.c} 0`
+      padding: `${variables.padding.c} 0`,
+      width: '100%'
     }
   })),
   relatedJob: {
@@ -107,6 +108,7 @@ const styles = {
     [mixins.breakpoints.l]: {
       flexBasis: '0',
       flexGrow: '1',
+      maxWidth: '25%',
       padding: `0 0 ${variables.padding.c} ${variables.padding.d}`,
       ':first-child': {
         paddingLeft: '0'
