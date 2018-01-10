607d744ec1e5ab58f77c1cc823fdf9ca27bd29ae
Nick Collings <nick@collingo.com>
2018-01-10 18:06:13 +0000

    Update apply fetcher

--- src/app/pages/apply/fetchers.js
+++ src/app/pages/apply/fetchers.js
@@ -1,12 +1,5 @@
-const { Redirect } = require('@nudj/framework/errors')
-const { merge } = require('@nudj/library')
-
-const job = require('../../server/modules/job')
-const queries = require('../../server/lib/queries-mutations')
-
 const post = (props) => {
-  const { params, session, body } = props
-  const { jobId } = body
+  const { params, session } = props
   const { data } = session
 
   const [
@@ -15,9 +8,35 @@ const post = (props) => {
     referralId
   ] = params.companySlugJobSlugReferralId.split('+')
 
-  const gql = queries.CreateApplicationForPerson
+  const gql = `
+    mutation CreateApplicationForPerson (
+      $companySlug: String!
+      $jobSlug: String!
+      $person: ID!
+      $referral: ID = null
+    ) {
+      company: companyByFilters(filters: {
+        slug: $companySlug
+      }) {
+        id
+        slug
+        job: jobByFilters(filters: {
+          slug: $jobSlug
+        }) {
+          id
+          slug
+          application: createApplication(
+            person: $person,
+            referral: referral
+          ) {
+            id
+          }
+        }
+      }
+    }
+  `
 
-  const variables = { job: jobId, referral: referralId, person: data.person.id }
+  const variables = { companySlug, jobSlug, referral: referralId, person: data.person.id }
 
   return { gql, variables }
 }
