05d6977be92c18bf072f6c587f625e6903053923
Timothy Robinson <timmyrobby@gmail.com>
2018-02-16 12:16:00 +0000

    fixup! Update /apply and /nudj job pages to use new url structure

--- src/app/pages/nudj/router.js
+++ src/app/pages/nudj/router.js
@@ -17,11 +17,11 @@ const Router = ({
 
   router.getHandlers('/companies/:companySlug/jobs/:jobSlug/nudj', handleJobUrls, noDirectApplyNudj)
   router.postHandlers('/companies/:companySlug/jobs/:jobSlug/nudj', handleJobUrls, cacheApplyNudjSecret, ensureLoggedIn, deleteApplyNudjSecret, respondWithGql(fetchers.post))
-  router.getHandlers('/jobs/:companySlugJobSlugReferralId/nudj/:secret', handleJobUrls, checkApplyNudjSecret, ensureLoggedIn, deleteApplyNudjSecret, respondWithGql(fetchers.post))
+  router.getHandlers('/companies/:companySlug/jobs/nudj/:secret', handleJobUrls, checkApplyNudjSecret, ensureLoggedIn, deleteApplyNudjSecret, respondWithGql(fetchers.post))
 
   // Legacy urls
-  router.getHandlers('/jobs/:companySlugJobSlugReferralId/nudj', handleJobUrls, noDirectApplyNudj)
-  router.getHandlers('/jobs/:companySlugJobSlugReferralId/nudj/:secret', handleJobUrls, checkApplyNudjSecret, ensureLoggedIn, deleteApplyNudjSecret, respondWithGql(fetchers.post))
+  router.getHandlers('/jobs/:companySlugJobSlugReferralId/nudj', handleJobUrls)
+  router.getHandlers('/jobs/:companySlugJobSlugReferralId/nudj/:secret', handleJobUrls)
 
   return router
 }
