3b9ed5e126e65531ea32bcbf2f7f99a16f166f37
jgunson <jamie.gunson@googlemail.com>
2018-05-08 11:19:46 +0100

    Finalising copy

--- src/app/pages/request/fetchers.js
+++ src/app/pages/request/fetchers.js
@@ -5,12 +5,14 @@ const post = ({ body }) => {
       $lastName: String
       $email: String!
       $company: String
+      $jobUrl: String
     ) {
       requestAccess (
         firstName: $firstName
         lastName: $lastName
         email: $email
         company: $company
+        jobUrl: $jobUrl
       ) {
         success
       }
@@ -20,7 +22,8 @@ const post = ({ body }) => {
     firstName: body.first_name,
     lastName: body.last_name,
     email: body.email,
-    company: body.company_name
+    company: body.company_name,
+    jobUrl: body.job_url
   }
 
   return { gql, variables }
