21b7fbb488abd6a7bc5e5d096bf5517981787c08
Richard Palmer <rich@rdjpalmer.com>
2018-05-18 13:34:55 +0100

    minor: Add intercom tracking to nudj page referral generation

--- src/app/pages/nudj/fetchers.js
+++ src/app/pages/nudj/fetchers.js
@@ -1,3 +1,52 @@
+const { logger } = require('@nudj/library')
+const { intercom } = require('@nudj/library/lib/analytics')
+const omitBy = require('lodash/omitBy')
+const isNil = require('lodash/isNil')
+
+const logReferralToIntercom = async (data) => {
+  try {
+    const { user, company } = data
+    const name = user.firstName && user.lastName
+      ? `${user.firstName} ${user.lastName}`
+      : null
+    const lead = await intercom.lead.getBy({ email: user.email })
+
+    if (lead && lead.id) {
+      await intercom.lead.convertToUser({
+        id: lead.id,
+        email: user.email
+      })
+    } else {
+      const intercomUser = omitBy({
+        email: user.email,
+        name,
+        custom_attributes: {
+          lastJobReferredFor: `${company.job.title} at ${company.name}`
+        }
+      }, isNil)
+      await intercom.user.create(intercomUser)
+    }
+
+    await intercom.user.logEvent({
+      user: {
+        email: user.email,
+        name
+      },
+      event: {
+        name: 'new-referral',
+        metadata: {
+          jobTitle: company.job.title,
+          company: company.name
+        }
+      }
+    })
+  } catch (error) {
+    logger('error', 'Intercom error', error)
+  }
+
+  return data
+}
+
 const post = ({ params, session, query }) => {
   const { userId } = session
   const { companySlug, jobSlug } = params
@@ -10,6 +59,11 @@ const post = ({ params, session, query }) => {
       $person: ID!
       $parent: ID = null
     ) {
+      user {
+        firstName
+        lastName
+        email
+      }
       company: companyByFilters(filters: {
         slug: $companySlug
       }) {
@@ -23,7 +77,7 @@ const post = ({ params, session, query }) => {
           slug
           title
           bonus
-          referral: createReferralForUser(
+          referral: getOrCreateReferralForUser(
             person: $person,
             parent: $parent
           ) {
@@ -40,7 +94,11 @@ const post = ({ params, session, query }) => {
     person: userId
   }
 
-  return { gql, variables }
+  return {
+    gql,
+    variables,
+    transformData: logReferralToIntercom
+  }
 }
 
 module.exports = {
