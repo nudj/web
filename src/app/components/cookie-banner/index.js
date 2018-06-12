b6b503f90a64c5442dce3a39c816042d2b0fb0be
Timothy Robinson <timmyrobby@gmail.com>
2018-06-12 14:30:40 +0100

    minor: Add cookie banner component
    patch: Update CookieBanner to avoid unneeded re-rendering

---
+++ src/app/components/cookie-banner/index.js
@@ -0,0 +1,77 @@
+const React = require('react')
+const cookies = require('js-cookie')
+
+const { Banner, Text, Button, Link, Align } = require('@nudj/components')
+
+const { css } = require('@nudj/components/lib/css')
+
+const style = require('./style.css')
+
+class CookieBanner extends React.Component {
+  state = {
+    shouldRender: false
+  }
+
+  componentDidMount () {
+    if (!cookies.get('cookiesEnabled')) {
+      this.setState({
+        shouldRender: true
+      })
+    }
+  }
+
+  handleAccept = () => {
+    this.setState({
+      shouldRender: false
+    }, () => {
+      cookies.set('cookiesEnabled', true)
+    })
+  }
+
+  handleDecline = () => {
+    this.setState({
+      shouldRender: false
+    }, () => {
+      cookies.set('cookiesEnabled', false)
+    })
+  }
+
+  render () {
+    const { onClickAccept, onClickDecline } = this.props
+    const { shouldRender } = this.state
+
+    if (!shouldRender) return null
+
+    return (
+      <Banner style={style.root}>
+        <Text style={style.text}>
+          We&apos;d like to use cookies to give you a great experience. Find out more in{' '}
+          <a className={css(style.privacyLink)} href='https://help.nudj.co/pricing-privacy-and-terms/nudj-privacy-policy'>
+            our privacy policy
+          </a>.
+        </Text>
+        <Button
+          id='accept-cookies'
+          onClick={this.handleAccept}
+          style={style.button}
+          nonsensitive
+          volume='cheer'
+          subtle
+        >
+          Okay
+        </Button>
+        <Button
+          style={style.button}
+          onClick={this.handleDecline}
+          nonsensitive
+          volume='murmur'
+          subtle
+        >
+          No thanks
+        </Button>
+      </Banner>
+    )
+  }
+}
+
+module.exports = CookieBanner
---
+++ src/app/components/cookie-banner/style.css.js
@@ -0,0 +1,68 @@
+const { StyleSheet, utilities, colors, sizes, typography } = require('@nudj/components/lib/css')
+
+const breakpoint = '38rem'
+
+const styleSheet = StyleSheet.create({
+  root: {
+    zIndex: 1,
+    position: 'fixed',
+    left: '50%',
+    transform: 'translateX(-50%)',
+    width: 'calc(100% - 2rem)',
+    maxWidth: '67.25rem',
+    bottom: '1rem',
+    paddingTop: '1.5rem',
+    paddingBottom: '1.5rem',
+    borderRadius: '0.4rem',
+    boxShadow: utilities.boxShadow[20].wide,
+    [`@media(min-width: ${breakpoint})`]: {
+      display: 'flex',
+    }
+  },
+  text: {
+    display: 'block',
+    [`@media(min-width: ${breakpoint})`]: {
+      display: 'inline',
+      marginRight: '1rem',
+      flexGrow: 1
+    }
+  },
+  privacyLink: {
+    ...typography.type.regular,
+    display: 'inline-block',
+    fontWeight: typography.fontWeight.regular,
+    textAlign: 'center',
+    transition: 'color 150ms',
+    borderWidth: 0,
+    cursor: 'pointer',
+    textDecoration: 'underline',
+    backgroundColor: 'transparent',
+    borderColor: 'transparent',
+    color: colors.primary,
+    textDecoration: 'underline',
+    ':hover': {
+      color: colors.primaryLight
+    },
+    ':focus': {
+      color: colors.primaryLight
+    }
+  },
+  button: {
+    flexShrink: 1,
+    paddingTop: 0,
+    paddingBottom: 0,
+    paddingLeft: 0,
+    paddingRight: 0,
+    marginTop: '1rem',
+    marginRight: '1rem',
+    whiteSpace: 'nowrap',
+    ':last-child': {
+      marginRight: 0,
+    },
+    [`@media(min-width: ${breakpoint})`]: {
+      marginTop: 0
+    }
+  },
+})
+
+module.exports = styleSheet
