b7b42be5a688bcd6c2111b62d66fc66e6f340cb4
jgunson <jamie.gunson@googlemail.com>
2018-07-11 10:38:26 +0100

    patch: Update page title to match header

--- src/app/pages/hirer/index.js
+++ src/app/pages/hirer/index.js
@@ -8,7 +8,7 @@ const RandomHover = require('../../components/random-hover')
 const Video = require('../../components/video')
 
 const Hirer = (props) => {
-  const pageTitle = `Supercharge your referral scheme with nudj`
+  const pageTitle = `Better manage your employee referral scheme and hire more great people`
   const pageDescription = `nudj makes it effortless for you and your team to ask for, track, and reward referrals.`
   const hireUrl = `${props.app.hire.protocol}://${props.app.hire.hostname}/`
   const style = getStyle()
