ed77a5d6e6024a657de3a89f7b38d4dd9be7df36
Nick Collings <nick@collingo.com>
2018-07-24 14:15:32 +0100

    Add crazy logs to page component

--- src/app/components/page/index.js
+++ src/app/components/page/index.js
@@ -1,13 +1,22 @@
+console.log(50)
 const React = require('react')
+console.log(51)
 
 const getStyle = require('./style.css')
+console.log(52)
 const ScrollTop = require('../scroll-top')
+console.log(53)
 const Notification = require('../notification')
+console.log(54)
 const Footer = require('../footer')
+console.log(55)
 const CookieBanner = require('../cookie-banner')
+console.log(56)
 
 const Page = (props) => {
+  console.log(57)
   const style = getStyle()
+  console.log(58)
 
   return (
     <ScrollTop ignore={props.history.action === 'REPLACE'}>
@@ -22,5 +31,8 @@ const Page = (props) => {
     </ScrollTop>
   )
 }
+console.log(59)
 
 module.exports = Page
+
+console.log(510)
