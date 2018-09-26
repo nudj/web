8e82b5c9c43dd51db248fc378121a4215a5fa73d
Timothy Robinson <timmyrobby@gmail.com>
2018-09-26 17:59:42 +0100

    minor: Replace `hire.nudj.co/welcome` links with `/hire.nudj.co/`

--- src/app/pages/home/index.js
+++ src/app/pages/home/index.js
@@ -147,7 +147,7 @@ const RandomHoverButton = ({ style, ...props }) => {
 const HomePage = (props) => {
   const pageTitle = `Better manage your employee referral scheme and hire more great people`
   const pageDescription = `nudj makes it effortless for you and your team to ask for, track, and reward referrals.`
-  const hireUrl = `${props.app.hire.protocol}://${props.app.hire.hostname}/welcome`
+  const hireUrl = `${props.app.hire.protocol}://${props.app.hire.hostname}`
 
   const {
     poorlyCommunicatedConversation,
