f750d30f00334d8d51c701b032d558ef2e2407cc
Nick Collings <nick@collingo.com>
2018-10-03 17:03:58 +0100

    patch: Update api port, update dev server port, improve yank

--- src/app/server/lib/request.js
+++ src/app/server/lib/request.js
@@ -4,26 +4,26 @@ const get = require('lodash/get')
 const logger = require('@nudj/framework/logger')
 
 function request (query, variables) {
-  return libRequest(`http://api:82/`, {
+  return libRequest(`http://${process.env.API_HOST}:${process.env.API_PORT}/`, {
     method: 'post',
     data: {
       query,
       variables
     }
   })
-  .then(data => {
-    if (data.errors) {
-      data.errors.forEach(error => logger.log('error', error.message, query, variables))
-      throw new Error(data.errors[0].message)
-    }
-    return data.data
-  })
-  .catch(error => {
-    logger.log('debug', JSON.parse(get(error, 'response.config.data', '{}')).query)
-    logger.log('debug', JSON.stringify(JSON.parse(get(error, 'response.config.data', '{}')).variables, null, 2))
-    get(error, 'response.data.errors', []).forEach(error => logger.log('error', error))
-    throw error
-  })
+    .then(data => {
+      if (data.errors) {
+        data.errors.forEach(error => logger.log('error', error.message, query, variables))
+        throw new Error(data.errors[0].message)
+      }
+      return data.data
+    })
+    .catch(error => {
+      logger.log('debug', JSON.parse(get(error, 'response.config.data', '{}')).query)
+      logger.log('debug', JSON.stringify(JSON.parse(get(error, 'response.config.data', '{}')).variables, null, 2))
+      get(error, 'response.data.errors', []).forEach(error => logger.log('error', error))
+      throw error
+    })
 }
 
 function openRequest (url, options) {
