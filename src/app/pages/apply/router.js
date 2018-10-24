e38c7438516fa10655d9c88e5ff1d7b4830c2f62
Timothy Robinson <timmyrobby@gmail.com>
2018-10-24 15:14:17 +0100

    minor: Replace `/apply/complete` page with `/apply` POST route

--- src/app/pages/apply/router.js
+++ src/app/pages/apply/router.js
@@ -1,17 +1,13 @@
 const createRouter = require('@nudj/framework/router')
 
 const fetchers = require('./fetchers')
-const {
-  handleJobUrls,
-  checkApplyNudjSecret,
-  deleteApplyNudjSecret
-} = require('../../server/lib/middleware')
+const { handleJobUrls } = require('../../server/lib/middleware')
 
 const Router = ({ respondWithGql }) => {
   const router = createRouter()
 
   router.getHandlers('/companies/:companySlug/jobs/:jobSlug/apply', handleJobUrls, respondWithGql(fetchers.get))
-  router.getHandlers('/companies/:companySlug/jobs/:jobSlug/apply/:secret', handleJobUrls, checkApplyNudjSecret, deleteApplyNudjSecret, respondWithGql(fetchers.get))
+  router.postHandlers('/companies/:companySlug/jobs/:jobSlug/apply', respondWithGql(fetchers.post))
 
   // Legacy urls
   router.getHandlers('/jobs/:companySlugJobSlugReferralId/apply', handleJobUrls)
